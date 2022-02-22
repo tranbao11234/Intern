c4a05ca8ef5934a3603203858a75315610ecc2a1
tranbao11234 <tranbao11234@gmail.com>
2022-02-22 11:35:34 +0700

    first project

---
+++ src/example.js
@@ -0,0 +1,13 @@
+import React from "react";
+// function example(){
+//     return <h1>Hello World</h1>;
+// }
+// export default example;
+
+export default class Example extends React.Component{
+    render(){
+        return (
+        <h1>Hello Bảo nhỏ</h1>
+        );
+    }
+}
